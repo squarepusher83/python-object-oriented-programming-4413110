4143e529069e6c1f162ab56b394b6ec7e84721c5
squarepusher83 <58606714+squarepusher83@users.noreply.github.com>
2024-07-15 21:52:34 +0000

    Chapter 1 file updates

--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -11,17 +11,29 @@ class Book:
         self.author = author
         self.pages = pages
         self.price = price
-    # TODO: create instance methods
+        self.__secret = "This is a secret attribute"
 
+    # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+    
+    def setdiscount(self, amount):
+        self._discount = amount
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "Leo Tolstoy", 1225,39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
 # TODO: print the price of book1
-
+#print(b1.getprice())
 
 # TODO: try setting the discount
-
+# print(b2.getprice())
+# b2.setdiscount(0.25)
+# print(b2.getprice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2._Book__secret)
